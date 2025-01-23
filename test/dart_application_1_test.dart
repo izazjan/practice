ba1914d303b32dd9e0d9276e3441d610ec11e9fb
izazjan <izaz70001@gmail.com>
2025-01-24 03:50:51 +0500

    Update dart_application_1_test.dart

--- test/dart_application_1_test.dart
+++ test/dart_application_1_test.dart
@@ -2,6 +2,8 @@ import 'package:dart_application_1/dart_application_1.dart';
 import 'package:test/test.dart';
 
 void main() {
+  int age = 20;
   print('izaz');
   print(' khan');
+  print('age: $age');
 }
