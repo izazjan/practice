e8b83bc6fc0cbefb1107bc55ff9d8a1a7a4202a6
izazjan <izaz70001@gmail.com>
2025-01-24 03:15:16 +0500

    Update dart_application_1_test.dart

--- test/dart_application_1_test.dart
+++ test/dart_application_1_test.dart
@@ -2,7 +2,5 @@ import 'package:dart_application_1/dart_application_1.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
+  print('izaz');
 }
